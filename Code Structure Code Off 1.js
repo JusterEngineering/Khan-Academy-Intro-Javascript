5c8eba8d35d6b9e07438999555ee96ff45d33726
Justyn Johur <justynjohur@gmail.com>
2018-12-04 14:53:13 -0800

    Create Code Structure Code Off 1.js

---
+++ Code Structure Code Off 1.js
@@ -0,0 +1,23 @@
+background(43, 171, 0);
+var add = function(num1, num2) {
+    return num1 + num2;
+};
+var subtract = function(num1, num2) {
+    return num1 - num2;
+};
+var multiply = function(num1, num2) {
+    return num1 * num2;
+};
+var divide = function(num1, num2) {
+    return num1/num2;
+};
+
+fill(255, 255, 255);
+text("15 + 3 is " + add(15, 3), 10, 20);
+text("15 - 3 is " + subtract(15, 3), 10, 50);
+text("15 * 3 is " + multiply(15, 3), 10, 80);
+text("15 / 3 is " + divide(15, 3), 10, 110);
+
+text("8 + 4 is " + add(8, 4), 10, 170);
+text("8 - 4 is " + subtract(8, 4), 10, 200);
+text("8 * 4 is " + multiply(8, 4), 10, 230);
